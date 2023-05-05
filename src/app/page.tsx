ad532701ea3d1e19fbd7f6a4cc94c92f13b1621d
Stephen Hopkins <github@stephenhopkins.dev>
2023-05-05 13:08:09 +0300

    fix: main page spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,14 @@ import NextLink from "next/link";
 export default function Home() {
   return (
     <main className={styles.page_main}>
-      <Typography variant="h2" className={styles.text}>
-        Hello, I&apos;m Stephen Hopkins
-      </Typography>
-      <Typography variant="h4" className={styles.text}>
-        A full stack software engineer with extensive experience in Typescript, React, C# and .NET Core
-      </Typography>
+      <div className={styles.text}>
+        <Typography variant="h2">Hello, I&apos;m Stephen Hopkins</Typography>
+      </div>
+      <div className={styles.text}>
+        <Typography variant="h4">
+          A full stack software engineer with extensive experience in Typescript, React, C# and .NET Core
+        </Typography>
+      </div>
     </main>
   );
 }
